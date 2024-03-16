94d40aa30b5227bc6e348c8189135ff123b08afb
Ken Wekesa <wanyamak884@gmail.com>
2024-03-16 18:15:01 +0300

    changed Werkzeug version to 2.1.1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -10,3 +10,9 @@ package { 'flask':
   provider => 'pip3',
   require  => Package['python3-pip'],
 }
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
