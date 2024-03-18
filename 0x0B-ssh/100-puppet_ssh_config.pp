30a9f727f1860e557cadf3a8961c6c9e2284e399
Ken Wekesa <wanyamak884@gmail.com>
2024-03-18 16:01:31 +0300

    task 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,22 +1,11 @@
-# using Puppet to make changes to our configuration file. 
+# using Puppet to make changes to our configuration file.
 
-file_line { 'Declare identity file':
-  path => '/etc/ssh/ssh_config',
-  line => 'IdentityFile ~/.ssh/school',
-  match => '^IdentityFile',
-  ensure => present,
-  require => File['/etc/ssh/ssh_config'],
-}
-
-file_line { 'Turn off passwd auth':
-  path => '/etc/ssh/ssh_config',
-  line => 'PasswordAuthentication no',
-  match => '^PasswordAuthentication',
-  ensure => present,
-  require => File['/etc/ssh/ssh_config'],
-}
-
-# Ensure the ssh_config file exists
 file { '/etc/ssh/ssh_config':
-  ensure => file,
+        ensure  => present,
+        content =>"
+          # SSH client config
+          host*
+          IdentifyFile ~/.ssh/school
+          PasswordAuthentication no
+        ",
 }
