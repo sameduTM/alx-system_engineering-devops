23d8587f2c6494f770fe0779c1ca20dc2bc2412a
Ken Wekesa <wanyamak884@gmail.com>
2024-03-24 09:41:21 +0300

    last task on puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,19 +1,17 @@
 # using Puppet to make changes to our configuration file. 
 
-file_line { 'Declare identity file':
+file { 'Declare identity file':
   path => '/etc/ssh/ssh_config',
   line => 'IdentityFile ~/.ssh/school',
   match => '^IdentityFile',
   ensure => present,
-  require => File['/etc/ssh/ssh_config'],
 }
 
-file_line { 'Turn off passwd auth':
+file { 'Turn off passwd auth':
   path => '/etc/ssh/ssh_config',
   line => 'PasswordAuthentication no',
   match => '^PasswordAuthentication',
   ensure => present,
-  require => File['/etc/ssh/ssh_config'],
 }
 
 # Ensure the ssh_config file exists
