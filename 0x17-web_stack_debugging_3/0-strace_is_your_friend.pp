7c5aee138711590fa49b42f4c294b226cb263d48
Ken Wekesa <wanyamak884@gmail.com>
2024-05-09 01:01:19 +0300

    puppet lint check

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-#  Using strace, find out why Apache is returning a 500 error. Once you find the issue, fix it and then automate it using Puppet (instead of using Bash as you were previously doing).
+#  Using strace, find out why Apache is returning a 500 error. Fix it and then automate it using Puppet
 
 exec {'fix Apache Error':
     command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
