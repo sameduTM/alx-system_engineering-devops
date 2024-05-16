3dead5de64616d6ddbbc9a2274aae5cf41ddf189
Ken Wekesa <wanyamak884@gmail.com>
2024-05-16 11:46:56 +0300

    0. Sky is the limit, let's bring that limit higher

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,20 @@
 # fix our stack to handle all requests
-exec { 'extend nginx limit':
-  command  => "sudo sed -i 's/15/4096/g' /etc/default/nginx; sudo service nginx reload",
+
+exec { 'replace nginx ulimit':
+  command => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  path    => '/bin:/usr/bin:/sbin:/usr/sbin',
+  onlyif  => 'grep -q "ULIMIT=\"-n 15\"" /etc/default/nginx',
+  notify  => Exec['restart nginx'],
+  require => Package['nginx'],
 }
+
+exec { 'restart nginx':
+  command     => 'service nginx restart',
+  path        => '/bin:/usr/bin:/sbin:/usr/sbin',
+  refreshonly => true,
+}
+
+package { 'nginx':
+  ensure => installed,
+}
+
