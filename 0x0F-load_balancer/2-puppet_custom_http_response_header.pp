8c2c26f243133adf2acb2635fc5aac6b025a0128
Ken Wekesa <wanyamak884@gmail.com>
2024-06-15 22:29:06 +0300

    used less code

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,52 +1,26 @@
 # puppet_custom_http_response_header.pp
 
-node default {
-
-  # Ensure Nginx is installed
-  package { 'nginx':
-    ensure => installed,
-  }
-
-  # Ensure Nginx service is running and enabled at boot
-  service { 'nginx':
-    ensure    => running,
-    enable    => true,
-    subscribe => File['/etc/nginx/nginx.conf'],
-  }
-
-  # Ensure the hostname is available as a fact
-  Facter.add('hostname') do
-    setcode do
-      Facter::Core::Execution.exec('hostname')
-    end
-  end
-
-  # Configure Nginx to include the custom HTTP header
-  file { '/etc/nginx/nginx.conf':
-    ensure  => file,
-    content => template('nginx/nginx.conf.erb'),
-    notify  => Service['nginx'],
-  }
-
-  # Create a template for nginx.conf with the custom header
-  file { '/etc/puppetlabs/code/environments/production/modules/nginx/templates/nginx.conf.erb':
-    ensure  => file,
-    content => "
-    server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-
-        server_name _;
-        root /var/www/html;
-
-        location / {
-            try_files \$uri \$uri/ =404;
-            add_header X-Served-By <%= @hostname %>;
-        }
-    }
+exec {'update repo':
+  provider => shell,
+  command  => 'sudo apt-get -y update',
+  before   => Exec['install Nginx'],
 }
-",
-    notify  => Service['nginx'],
-  }
 
+exec {'install nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['add_header'],
+}
+
+exec { 'add custom header':
+  provider    => shell,
+  environment => ["HOST=${hostname}"],
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include
+   \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  before      => Exec['restart Nginx'],
+}
+
+exec { 'restart nginx service':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
